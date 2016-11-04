d65396248e44c9bdc5cbabc8c9adf6461d58db24
skliba <stefano.kliba@infinum.hr>
2016-11-04 04:44:31 +0100

    Finalized fragment

--- app/src/main/java/org/foi/androidworkshop/activities/MemeActivity.java
+++ app/src/main/java/org/foi/androidworkshop/activities/MemeActivity.java
@@ -1,15 +1,36 @@
 package org.foi.androidworkshop.activities;
 
 import org.foi.androidworkshop.R;
+import org.foi.androidworkshop.fragments.MemeFragment;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MemeActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_meme_input)
+    EditText etMemeInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meme);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.btn_send_meme)
+    public void sendMemeClicked() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.meme_fragment);
+        if (fragment instanceof MemeFragment) {
+            ((MemeFragment) fragment).setTopText(etMemeInput.getText().toString());
+        }
+
+    }
+
 }
--- app/src/main/java/org/foi/androidworkshop/fragments/MemeFragment.java
+++ app/src/main/java/org/foi/androidworkshop/fragments/MemeFragment.java
@@ -9,19 +9,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MemeFragment extends Fragment {
 
+
+    @BindView(R.id.tv_meme_text)
+    TextView tvMemeText;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meme, container);
+        ButterKnife.bind(this, view);
         return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void setTopText(String text) {
+        tvMemeText.setText(text);
     }
 }
